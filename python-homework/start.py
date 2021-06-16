973314a7673249a3b14eafb43e4fc15331875e03
Tibor <leeltib@gmail.com>
2021-06-16 08:41:05 +0200

    Python házik.

--- python-homework/start.py
+++ python-homework/start.py
@@ -3,10 +3,39 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('https://fonts.google.com/')
 
-keres = driver.find_element_by_id('mat-input-0')
-keres.send_keys('Anton')
+q = driver.find_element_by_id('mat-input-0')
+q.send_keys('Anton')
 
 submit = driver.find_element_by_xpath("//h1[contains(@class,'mat-text--title gmat-headline-6')]")
 submit.click()
 
+driver.close()
+
+
+# fontkereső függvény
+font = input("Melyik fontot szeretnéd megnézni? (pl. Asul) ")
+
+driver = webdriver.Chrome()
+driver.get('https://fonts.google.com/')
+
+q = driver.find_element_by_id('mat-input-0')
+q.send_keys(font)
+
+submit = driver.find_element_by_xpath(f"//h1[contains(text(),'{font}')]")
+submit.click()
+
 # driver.close()
+
+
+# megnyitás Firefox-ban
+
+driver = webdriver.Firefox()
+driver.get('https://fonts.google.com/')
+
+q = driver.find_element_by_id('mat-input-0')
+q.send_keys('Allan')
+
+submit = driver.find_element_by_xpath("//h1[contains(text(),'Allan')]")
+submit.click()
+
+driver.close()
