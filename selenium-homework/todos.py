02e0aa888527cc66fc3331a6f586f8f39d0a91c5
Tibor <leeltib@gmail.com>
2021-06-17 11:12:53 +0200

    Python selenium házik.

---
+++ selenium-homework/todos.py
@@ -0,0 +1,29 @@
+# 015 Feladat: Selenium adat kinyeréses feladatok
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://localhost:9999/todo.html')
+
+articels = driver.find_elements_by_xpath("//ul//span[@class='done-false']")
+
+row = 0
+for articel in articels:
+    row += 1
+    print(f"{row}. aktív bejegyzés: ", articel.text)
+
+
+# Ha kikapcsolunk bejegyzéseket:
+but_off1 = driver.find_element_by_xpath("/html/body/div/div/div/ul/li[2]/input")
+but_off2 = driver.find_element_by_xpath("/html/body/div/div/div/ul/li[4]/input")
+but_off1.click()
+but_off2.click()
+
+print('*' * 50)
+print("Ha érvénytelenítünk két sort:")
+articels = driver.find_elements_by_xpath("//ul//span[@class='done-false']")
+
+row = 0
+for articel in articels:
+    row += 1
+    print(f"{row}. aktív bejegyzés: ", articel.text)
