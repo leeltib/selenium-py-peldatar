385a31689bb1e95c6bddf507a4d5b86e74beb80f
Tibor <leeltib@gmail.com>
2021-06-20 23:58:43 +0200

    Python selenium házik.

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -6,16 +6,10 @@ driver = webdriver.Chrome()
 driver.get('http://localhost:9999/trickyelements.html')
 
 elements = []
-element1 = driver.find_element_by_id('element1')
-elements.append(element1)
-element2 = driver.find_element_by_id('element2')
-elements.append(element2)
-element3 = driver.find_element_by_id('element3')
-elements.append(element3)
-element4 = driver.find_element_by_id('element4')
-elements.append(element4)
-element5 = driver.find_element_by_id('element5')
-elements.append(element5)
+for i in range(5):
+    k = str(i + 1)
+    element = driver.find_element_by_id("element" + k)
+    elements.append(element)
 
 # print(elements)
 sorszam = 0
