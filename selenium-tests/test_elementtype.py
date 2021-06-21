2801f894bca2ff90a174756d8b2703a9eb1ae624
Tibor <leeltib@gmail.com>
2021-06-21 22:48:17 +0200

    HF - Actions

--- selenium-tests/test_elementtype.py
+++ selenium-tests/test_elementtype.py
@@ -15,26 +15,30 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=option
 driver.get('http://localhost:9999/trickyelements.html')
 
 test_elements = []
-for i in range(5):
-    k = str(i + 1)
-    element = driver.find_element_by_id("element" + k)
-    test_elements.append(element)
+def test_elem1():
+    for i in range(5):
+        k = str(i + 1)
+        element = driver.find_element_by_id("element" + k)
+        test_elements.append(element)
 
 # print(elements)
-test_sorszam = 0
-for test_elem in test_elements:
-    test_sorszam += 1
-    test_elem.click()
-    test_result = driver.find_element_by_xpath('//label[@id="result"]')
-    if test_result.text == f"{test_elem.text} was clicked":
-        print(f"Az első button típusú elemünk: {test_elem.text}.")
-        print("Az elemek listája alatti szöveg helyes.")
-        break
-    elif test_result.text == "[Click any button]":
-        print(f"A(z) {test_sorszam}. elem nem gomb.")
-    else:
-        print(f"Az első button típusú elemünk: {test_elem.text}.")
-        print("Az elemek listája alatti szöveg viszont helytelen.")
-        break
+def test_elem2():
+    test_sorszam = 0
+    for test_elem in test_elements:
+        test_sorszam += 1
+        test_elem.click()
+        test_result = driver.find_element_by_xpath('//label[@id="result"]')
+        if test_result.text == f"{test_elem.text} was clicked":
+            print(f"Az első button típusú elemünk: {test_elem.text}.")
+            print("Az elemek listája alatti szöveg helyes.")
+            break
+        elif test_result.text == "[Click any button]":
+            print(f"A(z) {test_sorszam}. elem nem gomb.")
+        else:
+            print(f"Az első button típusú elemünk: {test_elem.text}.")
+            print("Az elemek listája alatti szöveg viszont helytelen.")
+            break
 
+test_elem1()
+test_elem2()
 
